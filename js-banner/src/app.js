daf5640cf883de55282372b0328617bb5436de25
niravkpatel28 <nirav.patel@contentstack.com>
2023-04-25 12:48:33 +0530

    JS counter and styling

--- js-banner/src/app.js
+++ js-banner/src/app.js
@@ -1,7 +1,9 @@
 import { createBanner } from "./components/Banner.js";
 import { createNavigation } from "./components/Navigation.js";
+import { createCounter } from "./components/Counter.js";
 
 console.log(createNavigation());
 
 document.body.appendChild(createNavigation());
-document.body.appendChild(createBanner());
+// document.body.appendChild(createBanner());
+document.body.appendChild(createCounter());
--- js-banner/src/components/Counter.js
+++ js-banner/src/components/Counter.js
@@ -1,1 +1,42 @@
-const count = 1;
+let counter = 1;
+
+function createCounter() {
+  const counterContainer = document.createElement("div");
+  const count = document.createElement("h2");
+  const incButton = document.createElement("button");
+  const dcrButton = document.createElement("button");
+
+  // add classes and ID
+  counterContainer.classList.add("counter");
+  count.id = "countValue"; // helps to identify the element on page
+
+  // add inner text elements
+
+  count.innerText = `count: ${counter}`;
+  incButton.innerText = "+";
+  dcrButton.innerText = "-";
+  // add event listeners
+  incButton.onclick = () => {
+    counter = counter + 1;
+    updateCount();
+  };
+
+  dcrButton.addEventListener("click", () => {
+    counter = counter - 1;
+    updateCount();
+  });
+
+  // add all elements to counterContainer
+  counterContainer.appendChild(count);
+  counterContainer.appendChild(incButton);
+  counterContainer.appendChild(dcrButton);
+
+  return counterContainer;
+}
+
+function updateCount() {
+  const displayCount = document.getElementById("countValue");
+  displayCount.innerText = `Count: ${counter}`;
+}
+
+export { createCounter };
